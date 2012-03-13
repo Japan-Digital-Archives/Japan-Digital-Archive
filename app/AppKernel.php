d0de03e16c7fa8f5e319413c3a5ca6c5aa96dec1
Luis Brandao <luisbrandao@gmail.com>
2012-03-13 16:58:11 -0400

    Removed old UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new Zeega\JDABundle\ZeegaJDABundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Zeega\UserBundle\ZeegaUserBundle(),
         );
 
 
--- src/Zeega/UserBundle/ZeegaUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zeega\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ZeegaUserBundle extends Bundle
-{
-}
