0dfa3e1d16182a0415185f30565cf633abba6678
James Burns <jameburn@gmail.com>
2012-06-04 19:55:34 +0000

    fix add item to collection

--- web/jda/js/app/collections/items.my-collections-drawer.js
+++ web/jda/js/app/collections/items.my-collections-drawer.js
@@ -20,19 +20,18 @@
 			};
 
 			//show 3 thumbnails by default in collections drawer
-			this.showThumbnailCount = 3;
+			this.showThumbnailCount = 4;
 			
 			
 			
 		},
 
-		render : function()
-		{
+		render : function(){
 			var _this = this;
 
 			//Render collection list in drop-down menu
 			$(_this.el).find('.dropdown-menu').empty();
-
+	
 			_.each( _.toArray(this.collection), function(item){
 				var itemView = '<li class="zeega-collection-list-item" id="'+item.id+'"><a href=".">'+item.get('title')+'</a></li>';
 				$(_this.el).find('.dropdown-menu').append(itemView);
@@ -52,6 +51,7 @@
 				
 				this.activeCollection = new Items.Model();
 				this.activeCollection.set({title:$('#zeega-my-collections-active-collection').text()}); 
+				
 			} 
 			/* 
 				Otherwise make the first collection in the list the active one for the
@@ -60,57 +60,85 @@
 			else {
 				var activeCollectionID = $(this.el).find('.zeega-collection-list-item').first().attr("id");
 				this.switchActiveCollection(activeCollectionID);
-
-			
 			}
+			
+			
 
 			$(this.el).find('#zeega-my-collections-items').droppable({
 			    accept : '.list-fancymedia',
 			    hoverClass : 'zeega-my-collections-items-dropping',
 			    tolerance : 'pointer',
 
-
-			    drop : function( event, ui )
-			    {
+			    drop : function( event, ui ){
+			    
 			    	//TODO -- Check whether user is logged in - if not then log them in before adding
-			      
-			      var kids = _.toArray(_this.activeCollection.get('child_items'));
-			      kids.push(jda.app.draggedItem.toJSON());
-			      _this.activeCollection.set('child_items', kids);
-
-			      _this.activeCollection.save(
-			      		{},
-				      	{success : function(model, response)
-						{ 
-							//_this.activeCollection = model;
-							_this.render();
-						},
-						error : function(model, response){
-
-						}}
-			      );
-			      
-			      
-			      ui.draggable.draggable('option','revert',false);
-
+					if(_.difference([jda.app.draggedItem.id],_.pluck(_this.activeCollection.attributes.child_items,"id")).length==0){
+						console.log('duplicate');
+						
+					}
+					else {
+						
+						$(_this.el).find('#zeega-my-collections-items').addClass('zeega-my-collections-items-dropping');
+					
+						_this.activeCollection.attributes.child_items.push(jda.app.draggedItem.toJSON());
+						
+					
+						
+						_this.renderCollectionPreview(_this.activeCollection);
+						  
+						var itemId=jda.app.draggedItem.id;
+						
+						_this.activeCollection.url=jda.app.apiLocation + 'api/collections/' + _this.activeCollection.id+'/items';
+				
+						console.log(_this.activeCollection);
+						_this.activeCollection.save({newItemIDS:[itemId ]},
+								{
+									success : function(model, response){ 
+										
+										$(_this.el).find('#zeega-my-collections-items').removeClass('zeega-my-collections-items-dropping');
+										_this.renderCollectionPreview(model);	
+									},
+									error : function(model, response){
+		
+									}
+								}
+							);
+						}
+					ui.draggable.draggable('option','revert',false);
+					
 			    }
 			});
 			  
 	
 			return this;
 		},
+		
 		switchActiveCollection :function(activeCollectionID){
 			var _this = this;
-			this.activeCollection = new Items.Model({id:activeCollectionID});
-
 			
+			$('#zeega-my-collections-items').spin();
+			this.activeCollection = new Items.Model({id:activeCollectionID});
 			this.activeCollection.fetch(
 			{
 				//Display the first x thumbnails in the collection
 				success : function(model, response)
 				{ 
+					_this.renderCollectionPreview(model);
+					$('#zeega-my-collections-items').spin('false');
+				},
+				error : function(model, response)
+				{ 
+					console.log('Error getting active collection for collections drawer');
+
+				}
+			});
+			
+		},
+		
+		renderCollectionPreview: function(model){
 					var title = model.get('title');
-					var remainingItems = model.get('child_items').length - _this.showThumbnailCount;
+					var remainingItems = model.get('child_items').length - this.showThumbnailCount;
+					var _this=this;
 					
 					$('#zeega-my-collections-items-thumbs').empty();
 					$('#zeega-my-collections-active-collection').text(model.get('title'));
@@ -135,8 +163,8 @@
 					
 					var kids = _.toArray(model.get('child_items'));
 					
-					for (var i=0;i<Math.min(_this.showThumbnailCount, kids.length);i++){
-						var item = kids[i];
+					for (var i=1;i<Math.min(this.showThumbnailCount, kids.length);i++){
+						var item = kids[kids.length-i];
 						
 						var itemView = new Items.Views.Thumb({model:new Items.Model(item)});
 						itemView.model.set({thumbnail_width:120, thumbnail_height:80});
@@ -144,21 +172,9 @@
 						$('#zeega-my-collections-items-thumbs').append(itemView.el);
 
 					}
-
-					
-				},
-				error : function(model, response)
-				{ 
-					console.log('Error getting active collection for collections drawer');
-
-				}
-			});
-			
 		},
 		
-			
-		getCollectionList : function()
-		{
+		getCollectionList : function(){
 			var _this = this;
 			
 			// fetch list of collections for drawer drop-down
