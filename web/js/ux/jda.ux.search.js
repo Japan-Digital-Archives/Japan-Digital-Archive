ec8e3720c7da2d7317060177c9cf2f5b1b396d62
James Burns <jameburn@gmail.com>
2012-06-13 22:39:11 +0000

    add browser/os detect to bug reporting

--- web/js/ux/jda.ux.search.js
+++ web/js/ux/jda.ux.search.js
@@ -2,6 +2,133 @@ $(document).ready(function(){
   
    jda.app.redrawLayout();
   
+	var BrowserDetect = {
+	init: function () {
+		this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+		this.version = this.searchVersion(navigator.userAgent)
+			|| this.searchVersion(navigator.appVersion)
+			|| "an unknown version";
+		this.OS = this.searchString(this.dataOS) || "an unknown OS";
+	},
+	searchString: function (data) {
+		for (var i=0;i<data.length;i++)	{
+			var dataString = data[i].string;
+			var dataProp = data[i].prop;
+			this.versionSearchString = data[i].versionSearch || data[i].identity;
+			if (dataString) {
+				if (dataString.indexOf(data[i].subString) != -1)
+					return data[i].identity;
+			}
+			else if (dataProp)
+				return data[i].identity;
+		}
+	},
+	searchVersion: function (dataString) {
+		var index = dataString.indexOf(this.versionSearchString);
+		if (index == -1) return;
+		return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+	},
+	dataBrowser: [
+		{
+			string: navigator.userAgent,
+			subString: "Chrome",
+			identity: "Chrome"
+		},
+		{ 	string: navigator.userAgent,
+			subString: "OmniWeb",
+			versionSearch: "OmniWeb/",
+			identity: "OmniWeb"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Apple",
+			identity: "Safari",
+			versionSearch: "Version"
+		},
+		{
+			prop: window.opera,
+			identity: "Opera",
+			versionSearch: "Version"
+		},
+		{
+			string: navigator.vendor,
+			subString: "iCab",
+			identity: "iCab"
+		},
+		{
+			string: navigator.vendor,
+			subString: "KDE",
+			identity: "Konqueror"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Firefox",
+			identity: "Firefox"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Camino",
+			identity: "Camino"
+		},
+		{		// for newer Netscapes (6+)
+			string: navigator.userAgent,
+			subString: "Netscape",
+			identity: "Netscape"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "MSIE",
+			identity: "Explorer",
+			versionSearch: "MSIE"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Gecko",
+			identity: "Mozilla",
+			versionSearch: "rv"
+		},
+		{ 		// for older Netscapes (4-)
+			string: navigator.userAgent,
+			subString: "Mozilla",
+			identity: "Netscape",
+			versionSearch: "Mozilla"
+		}
+	],
+	dataOS : [
+		{
+			string: navigator.platform,
+			subString: "Win",
+			identity: "Windows"
+		},
+		{
+			string: navigator.platform,
+			subString: "Mac",
+			identity: "Mac"
+		},
+		{
+			   string: navigator.userAgent,
+			   subString: "iPhone",
+			   identity: "iPhone/iPod"
+	    },
+		{
+			string: navigator.platform,
+			subString: "Linux",
+			identity: "Linux"
+		}
+	]
+
+	};
+		BrowserDetect.init();
+		console.log(BrowserDetect);
+
+
+
+
+
+	/***************************************************/
+	/*************** HEADER ****************************/
+	/***************************************************/
+
 
 	/*************** USER LOGIN ************************/
 	
@@ -20,8 +147,6 @@ $(document).ready(function(){
 		return false;
 	});
 	
-
-
 	
 	$('#user-modal').bind('close',function(){$("#user-modal-close").trigger('click');});
 	
@@ -36,12 +161,13 @@ $(document).ready(function(){
 		
 	});
 	
-		
+	
 	
 	/************  BUG REPORT **********************/
 	
 	
 	$('.bug-report').click(function(e){e.stopPropagation();});
+	
 	$('.bug-report').parent().click(function(){
 		$('.bug-unsubmitted').show();
 		$('.bug-submitted').hide();
@@ -60,6 +186,10 @@ $(document).ready(function(){
 			hash: window.location.hash.substr(1),
 			description: $('.bug-description').val(),
 			email: $('.bug-email').val(),
+			browser: BrowserDetect.browser,
+			version: BrowserDetect.version,
+			os:BrowserDetect.OS,
+			login:sessionStorage.getItem('user')
 		
 		});
 		
@@ -69,23 +199,27 @@ $(document).ready(function(){
 		$('.bug-unsubmitted').fadeOut('fast',function(){
 				$('.bug-submitted').fadeIn();
 		});
-		
-		
-	
 	
 	});
-  /*************** LANGUAGE TOGGLE ************************/
-    
-  $('#jda-language-toggle').find('.btn').click(function(){
-    if(!$(this).hasClass('active')){
-      $('#jda-language-toggle').find('.btn').removeClass('active');
-      $(this).addClass('active');
-      if($(this).data('language')=='en') window.location =  window.location.href.replace('ja/search','en/search');
-      else window.location =  window.location.href.replace('en/search','ja/search');
-    }
-  });
+	
+	
 
-
+	/*************** LANGUAGE TOGGLE ************************/
+	$('#jda-language-toggle').find('.btn').click(function(){
+		if(!$(this).hasClass('active')){
+			console.log('switching languages');
+			$('#jda-language-toggle').find('.btn').removeClass('active');
+			$(this).addClass('active');
+			if($(this).data('language')=='en') window.location =  window.location.href.replace('ja/home','en/home');
+			else window.location =  window.location.href.replace('en/home','ja/home');
+		}
+		
+	});
+	
+	
+	
+	
+	
 
   
   
