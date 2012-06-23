5f19362755eaf7c309f0714ad5fab7840b3c24a2
James Burns <jameburn@gmail.com>
2012-06-23 20:25:49 +0000

    fixes #153

--- web/js/app/jda.js
+++ web/js/app/jda.js
@@ -313,12 +313,13 @@ this.jda = {
 	showListView : function(){
 		console.log('switch to List view');
 
-		 $('#event-time-slider').hide();
+	    
 		$('#zeega-view-buttons .btn').removeClass('active');
 		$('#list-button').addClass('active');
 		
-		//Time slider disabled for now
-		//$('#event-time-slider').hide();
+
+		$('#zeega-right-column').show();
+		$('#event-time-slider').hide();
 		$('#zeega-results-count').removeClass('zeega-results-count-event');
 		$('#zeega-results-count').css('left', 0);
 		$('#zeega-results-count').css('z-index', 0);
@@ -337,7 +338,9 @@ this.jda = {
 	
 		$('#zeega-view-buttons .btn').removeClass('active');
 		$('#thumb-button').addClass('active');
-		 $('#event-time-slider').hide();
+		
+		$('#zeega-right-column').show();
+		$('#event-time-slider').hide();
 		$('#zeega-results-count').removeClass('zeega-results-count-event');
 		$('#zeega-results-count').css('left', 0);
 		$('#zeega-results-count').css('z-index', 0);
@@ -355,6 +358,8 @@ this.jda = {
 		console.log('switch to Event view');
 		$('#zeega-view-buttons .btn').removeClass('active');
 		$('#event-button').addClass('active');
+		
+		$('#zeega-right-column').hide();
 		$('#event-time-slider').show();
 		$('#zeega-results-count').addClass('zeega-results-count-event');
 		$('#zeega-results-count').offset( { top:$('#zeega-results-count').offset().top, left:10 } );
