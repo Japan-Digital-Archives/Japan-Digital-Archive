719dafd5ff5a0c154104809de7f7d41ccc80c0df
jbergen <jbergen@gmail.com>
2012-06-27 17:56:41 +0000

    homepage images scale without stacking ever. fixes #306

--- web/js/ux/jda.ux.home.js
+++ web/js/ux/jda.ux.home.js
@@ -230,6 +230,8 @@ $(document).ready(function(){
 		window.open(sessionStorage.getItem('hostname')+sessionStorage.getItem('directory')+sessionStorage.getItem('locale')+"/search?#view_type=thumb&collection="+$(this).data('id')+"&content=all&","_self");
 	
 	});
+	
+	
 	$(window).resize(function() {
       $('.jda-home-featured-collection').height(Math.max($(window).height()-50, 600));
   });
