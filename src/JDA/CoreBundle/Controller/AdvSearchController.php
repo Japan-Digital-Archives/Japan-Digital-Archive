29d58b2ab8007f9493465c849f83d2ecb2b6cb02
E.J. Bensing <ebensing@rm-dash-r.com>
2012-12-09 15:16:49 -0500

    try to fix the userid issue

--- src/JDA/CoreBundle/Controller/AdvSearchController.php
+++ src/JDA/CoreBundle/Controller/AdvSearchController.php
@@ -12,9 +12,19 @@ class AdvSearchController extends Controller
     {
     
     	$locale=$this->getRequest()->getLocale();
+        if(is_object($user)){
+    		$displayName = $user->getDisplayName();
+    		$userId = $user->getId();
+    	}
+    	else{
+    		$displayName='none';
+    		$userId=0;	
+    	}
 		return $this->render('JDACoreBundle:AdvSearch:advsearch.html.twig', array(
 					// last displayname entered by the user
 					'page'=> 'search',
+                    'displayname'=>$displayName,
+                    'userId'=>$userId,
 				));
     }
 }
--- src/JDA/CoreBundle/Controller/SeedSubmitController.php
+++ src/JDA/CoreBundle/Controller/SeedSubmitController.php
@@ -11,16 +11,36 @@ class SeedSubmitController extends Controller
     public function indexAction()
     {
     
+        if(is_object($user)){
+    		$displayName = $user->getDisplayName();
+    		$userId = $user->getId();
+    	}
+    	else{
+    		$displayName='none';
+    		$userId=0;	
+    	}
+    
 		return $this->render('JDACoreBundle:Forms:seedSubmit.html.twig', array(
 					'page'=> 'Seed Submission Form',
+                    'displayname'=>$displayName,
+                    'userId'=>$userId
 				));
     }
     
     public function testimonialAction()
     {
-        
+        if(is_object($user)){
+    		$displayName = $user->getDisplayName();
+    		$userId = $user->getId();
+    	}
+    	else{
+    		$displayName='none';
+    		$userId=0;	
+    	}
 		return $this->render('JDACoreBundle:Forms:testimonial.html.twig', array(
-        'page'=> 'Testimonial Submission Form',
+                    'page'=> 'Testimonial Submission Form',
+                    'displayname'=>$displayName,
+                    'userId'=>$userId
         ));
     }
 }
