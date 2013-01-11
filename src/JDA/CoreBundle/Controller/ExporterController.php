99e1e18aaef6436d2ec9e508b3981eab774865d5
E.J. Bensing <ebensing@rm-dash-r.com>
2013-01-11 18:28:34 -0500

    whitespace issues?

--- src/JDA/CoreBundle/Controller/ExporterController.php
+++ src/JDA/CoreBundle/Controller/ExporterController.php
@@ -11,15 +11,15 @@ class ExporterController extends Controller
     public function indexAction()
     {
     
-    	// $loggedUser = $this->get('security.context')->getToken()->getUser();
+        // $loggedUser = $this->get('security.context')->getToken()->getUser();
         $fileLoc = "lastExport.txt";
         if(!file_exists($fileLoc)) {
             file_put_contents($fileLoc, "1");
         }
         $lastItem = file_get_contents($fileLoc);
-		return $this->render('JDACoreBundle:SeedExport:export.html.twig', array(
-					'page'=> 'export',
+        return $this->render('JDACoreBundle:SeedExport:export.html.twig', array(
+                    'page'=> 'export',
                     'lastItem' => $lastItem,
-				));
+                ));
     }
 }
