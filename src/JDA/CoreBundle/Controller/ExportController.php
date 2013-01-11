76c8fbdd7a9749b84b55c30b1a14c1948f3b466b
E.J. Bensing <ebensing@rm-dash-r.com>
2013-01-11 18:09:25 -0500

    added intial mvc framework for export

---
+++ src/JDA/CoreBundle/Controller/ExportController.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+namespace JDA\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class ExportController extends Controller
+{
+    
+    public function indexAction()
+    {
+    
+    	// $loggedUser = $this->get('security.context')->getToken()->getUser();
+        $fileLoc = "lastExport.txt";
+        if(!file_exists($fileLoc)) {
+            file_put_contents($fileLoc, "1");
+        }
+        $lastItem = file_get_contents($fileLoc);
+		return $this->render('JDACoreBundle:SeedExport:export.html.twig', array(
+					'page'=> 'export',
+                    'lastItem' => $lastItem
+				));
+    }
+}
