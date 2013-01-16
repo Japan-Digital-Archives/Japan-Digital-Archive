5be16d643aaed2ed5bf17089aed9fd1883a3d408
E.J. Bensing <ebensing@rm-dash-r.com>
2013-01-16 16:55:34 +0000

    input correct first date

--- src/JDA/CoreBundle/Controller/ExporterController.php
+++ src/JDA/CoreBundle/Controller/ExporterController.php
@@ -14,7 +14,7 @@ class ExporterController extends Controller
         // $loggedUser = $this->get('security.context')->getToken()->getUser();
         $fileLoc = realpath("lastExport.txt");
         if(!file_exists($fileLoc)) {
-            file_put_contents($fileLoc, date('m/d/Y h:i:s a', time()));
+            file_put_contents($fileLoc, date('m/d/Y h:i:s a', strtotime('12/11/2012 1:51:00 pm')));
         }
 		$lastExport = file_get_contents($fileLoc);
         return $this->render('JDACoreBundle:SeedExport:export.html.twig', array(
