b66e8f42e5dc57a50555d8cf2aa4805eb7515d5a
E.J. Bensing <ebensing@rm-dash-r.com>
2013-02-17 16:19:58 -0500

    testing

--- src/Zeega/AdminBundle/AdminEntity/ItemAdmin.php
+++ src/Zeega/AdminBundle/AdminEntity/ItemAdmin.php
@@ -32,7 +32,7 @@ class ItemAdmin extends Admin
 			->add('tags', NULL, array('allow_add' => true, 'allow_delete' => true))
 			->add('attributes', NULL, array('allow_add' => true, 'allow_delete' => true))
 			->add('published')
-            ->add('date_created', 'date', array('required' => false, 'widget' => 'single_text', 'help' => 'Last Export Date: ' . $lastExport . '\n If created after this date, has not been sent to IA', 
+            ->add('date_created', 'date', array('required' => false, 'widget' => 'single_text', 'help' => 'Last Export Date: ' . $fileLoc . ' If created after this date, has not been sent to IA', 
                   'attr' => array('readonly' => true)))
 
         ;
