c8904f3351f993df9a09370019f4545ede72c340
EJ Bensing <ebensing@rm-dash-r.com>
2013-06-08 15:09:35 -0700

    made private testimonials anonymous
    this should make sure that testimonials that are supposed to be private actually are. fixes #533

--- web/js/app/views/items/jda.view.item.list.js
+++ web/js/app/views/items/jda.view.item.list.js
@@ -97,6 +97,14 @@
 			} else {
 				blanks["author"] = this.model.get("media_creator_realname");
 			}
+			if (this.model.get('attributes').privacy) {
+			    if (this.model.get('attributes').privacy == 'Hide') {
+			        blanks["author"] = "Anonymous";
+			    }
+			}
+			if (this.model.get("media_creator_realname") == "Anonymous") {
+			    blanks["author"] = "Anonymous";
+			}
 			if (this.model.get("media_type") == "Text" && this.model.get('description').length < this.model.get('text').length){
 				blanks["description"] = this.model.get('description') + '...';
 			}
