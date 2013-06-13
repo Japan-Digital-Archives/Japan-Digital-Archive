bca3ec0627c358249ff3254d64857727e89aa27e
EJ Bensing <ebensing@rm-dash-r.com>
2013-06-13 08:50:38 -0700

    fixed bug where admin did not display correct media type
    new headlines are stored in the DB as 'Headline' not 'News Headline' fixes #572

--- src/Zeega/AdminBundle/AdminEntity/ItemAdmin.php
+++ src/Zeega/AdminBundle/AdminEntity/ItemAdmin.php
@@ -15,7 +15,7 @@ class ItemAdmin extends Admin
 		$itemTypes = array('Video' => 'Video', 'Audio' => 'Audio',
 			'Text' => 'Text','Image' => 'Image','Tweet' => 'Tweet','Document' => 'Document',
 			'Website' => 'Website', 'News Article' => 'News Article', 'Testimonial' => 'Text',
-            'Broadcast' => 'Broadcast', 'News Headline' => 'News Headline');
+            'Broadcast' => 'Broadcast', 'News Headline' => 'Headline');
         $fileLoc = realpath("lastExport.txt");
 		$lastExport = file_get_contents($fileLoc);
         $formMapper
