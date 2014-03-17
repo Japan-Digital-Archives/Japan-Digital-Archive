bca93e2e0099d20ec53278a423c9368bc143ee4e
HakuG <harnekgulati@college.harvard.edu>
2014-03-17 13:33:09 -0400

    Working on Anonymous

--- web/js/app/views/fancybox/jda.view.fancybox._fancybox.js
+++ web/js/app/views/fancybox/jda.view.fancybox._fancybox.js
@@ -446,7 +446,7 @@
 
 							'</div>'+
 							'<p class="fancybox-editable title" style="text-transform: uppercase;"><%= title %></p>'+
-							'<p><p class="jda-item-author"><%= author %></p><span class="source"><a href="<%= sourceLink %>" target="_blank"><%= sourceText %></a></span></p>'+
+							'<p><span class=" creator fancybox-editable">'<?php include 'Author.php'; echo "$author"; ?>'</span> <span class="source"><a href="<%= sourceLink %>" target="_blank"><%= sourceText %></a></span></p>'+
 							'<div class="fancybox-right-column">'+
 
 								'<div class="description-wrapper">'+
