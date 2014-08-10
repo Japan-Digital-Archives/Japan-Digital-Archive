1ec65b5fbd9e1a17ef06af73b085149c23a18c25
HakuG <harnekgulati@college.harvard.edu>
2014-08-10 17:56:06 -0600

    Working on map

--- web/js/app/views/jda.view.locator-map.js
+++ web/js/app/views/jda.view.locator-map.js
@@ -23,7 +23,7 @@
 			else this.geoLocated=false;
 		
 			this.latlng = new L.LatLng(parseFloat(this.model.get('media_geo_latitude')),parseFloat(this.model.get('media_geo_longitude')));
-		
+			console.log(latlng);
 			var values = {
 				latitude : this.model.get('media_geo_latitude'),
 				longitude : this.model.get('media_geo_longitude')
