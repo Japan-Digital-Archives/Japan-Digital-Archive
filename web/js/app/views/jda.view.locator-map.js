3e4152747d67c541b944ee597dd6435e41a4cca5
HakuG <harnekgulati@college.harvard.edu>
2014-09-23 16:49:12 -0400

    got some shit

--- web/js/app/views/jda.view.locator-map.js
+++ web/js/app/views/jda.view.locator-map.js
@@ -20,13 +20,14 @@
 			this.cloudmade = new L.TileLayer(this.cloudmadeUrl, {maxZoom: 18, attribution: this.cloudmadeAttrib});
 			if(parseFloat(this.model.get('media_geo_latitude'))) this.geoLocated=true;
 			else this.geoLocated=false;
-		
+			if (this.geoLocated == true)
+			{
 			this.latlng = new L.LatLng(parseFloat(this.model.get('media_geo_latitude')),parseFloat(this.model.get('media_geo_longitude')));
-			
 			var values = {
 				latitude : this.model.get('media_geo_latitude'),
 				longitude : this.model.get('media_geo_longitude')
 			};
+			}
 			//use template to clone the database items into
 			var template = _.template( this.getTemplate() );
 
