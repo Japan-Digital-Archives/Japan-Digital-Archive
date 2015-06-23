fe37a376aeab9bf38c9a326b8e0fa3daa784512b
horak <alexamh9@gmail.com>
2015-06-23 14:52:30 -0400

    Conform indentation

--- web/js/app/views/items/jda.view.item.waku-area.js
+++ web/js/app/views/items/jda.view.item.waku-area.js
@@ -1,35 +1,35 @@
 (function(Browser) {
   
-  Browser.Items = Browser.Items || {};
-  Browser.Items.Views = Browser.Items || {};
+    Browser.Items = Browser.Items || {};
+    Browser.Items.Views = Browser.Items || {};
 
-  Browser.Items.Views.WakuArea = Backbone.View.extend({
+    Browser.Items.Views.WakuArea = Backbone.View.extend({
 
-    el : $('#jda-collection-waku-area'),
+        el : $('#jda-collection-waku-area'),
 
-    events: {
-      'click #create-waku-from-collection' : 'createWakuFromCollection'
-    },
+        events: {
+            'click #create-waku-from-collection' : 'createWakuFromCollection'
+        },
 
-    initialize: function()
-    {
-      this.render();
-    },
+        initialize: function()
+        {
+            this.render();
+        },
 
-    render: function() 
-    {
-      var _this = this;
-    },
+        render: function() 
+        {
+            var _this = this;
+        },
 
-    createWakuFromCollection: function() 
-    {
-      var WAKU_SOURCE_URL = "http://jdawaku.herokuapp.com/#en/dashboard/";
-      var collectionId    = this.model.id;
-      var url             = WAKU_SOURCE_URL + collectionId;
+        createWakuFromCollection: function() 
+        {
+            var WAKU_SOURCE_URL = "http://jdawaku.herokuapp.com/#en/dashboard/";
+            var collectionId    = this.model.id;
+            var url             = WAKU_SOURCE_URL + collectionId;
 
-      window.open(url, '_blank');
-    }
+            window.open(url, '_blank');
+        }
 
-  });
+      });
 
 })(jda.module("browser"));
