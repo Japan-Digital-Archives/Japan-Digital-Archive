e655b656ff75ea5da91545b81947e0ac6eb42b49
Javier Cuan-Martinez <jcuanm@gmail.com>
2016-08-19 11:35:33 -0400

    climbing up the DOM to see if I can find a different id/class to hook on to

--- web/js/app/views/items/jda.view.item.list.js
+++ web/js/app/views/items/jda.view.item.list.js
@@ -117,7 +117,7 @@
 			}
 
 			//console.log(this.model.get("description"));
-			var text = document.getElementsByClassName("VS-input-width-tester VS-interface").innerHTML;
+			var text = document.getElementById("search").textContent;
 			console.log(text);
 
 			$(this.el).html( _.template( template, blanks ) );
