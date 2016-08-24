a6f52e388811ee601dff58d211ee57bb6cd6c5b9
Javier Cuan-Martinez <jcuanm@gmail.com>
2016-08-23 20:07:45 -0400

    Splitting the strings up manually

--- web/js/app/views/items/jda.view.item.list.js
+++ web/js/app/views/items/jda.view.item.list.js
@@ -119,14 +119,28 @@
 			//console.log(this.model.get("description"));
 			var text = document.getElementById("search").textContent;
 			var old_arr = text.split("");
-			console.log(old_arr);
+			var substitute = "";
 			var new_arr = [];
 
 			for (var i = 0; i < old_arr.length; i++)
 			{
+				if (old_arr[i].match(/[A-Za-z]+/) != null)
+				{
+					substitute = substitue.concat(old_arr[i][0]);
+
+					if (old_arr[i + 1].match(/[A-Za-z]+/) == null)
+					{
+						new_arr.push(substitute);
+						substitute = "";
+					}
+				}
+				/*
 				var str = old_arr[i].match(/[A-Za-z]+/);
 				new_arr.push(str);
-			} 
+				*/
+			}
+
+			console.log(new_arr); 
 
 			var final_arr = [];
 
